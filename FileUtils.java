4eecab24c343e9e604816efdaadad7bd0e79cbcf
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2020-12-15 10:05:51 +0900

    Update FileUtils.java

--- FileUtils.java
+++ FileUtils.java
@@ -1,13 +1,13 @@
 package kr.co.saramin.lab.recruitassigntaskrepository.utils;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
+        import java.io.IOException;
+        import java.net.URISyntaxException;
+        import java.nio.charset.StandardCharsets;
+        import java.nio.file.Files;
+        import java.nio.file.Path;
+        import java.nio.file.Paths;
+        import java.nio.file.StandardOpenOption;
+        import java.util.List;
 
 public class FileUtils {
     /**
@@ -21,25 +21,57 @@ public class FileUtils {
         writeString2File(resultFilePath, dataString);
     }
 
-    /**
-     * String 데이터를 파일에 쓴다.
-     * @param resultFilePath 파일 경로
-     * @param dataString String 형태의 데이터
-     * @throws IOException
-     */
-    public static void writeString2File(Path resultFilePath, String dataString) throws IOException {
-        Files.write(resultFilePath, (dataString + System.lineSeparator()).getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND, StandardOpenOption.WRITE);
-    }
 
     /**
      * String 데이터를 파일에 쓴다.
-     * @param resultFilePathString 파일 경로
+     * @param resultFilePathString String 파일 경로
      * @param dataString String 형태의 데이터
      * @throws IOException
      */
     public static void writeString2File(String resultFilePathString, String dataString) throws IOException {
-
-        Files.write(Paths.get(resultFilePathString), (dataString + System.lineSeparator()).getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+        writeString2File(Paths.get(resultFilePathString), dataString);
     }
 
+    /**
+     * String 데이터 + 개행 을 파일에 쓴다.
+     * @param resultFilePathString String 파일 경로
+     * @param dataString String 형태의 데이터
+     * @throws IOException
+     */
+    public static void writeStringNewLine2File(String resultFilePathString, String dataString) throws IOException {
+        writeString2File(Paths.get(resultFilePathString), dataString + System.lineSeparator());
+    }
+
+    /**
+     * String 데이터 + 개행을 파일에 쓴다.
+     * @param resultFilePath Path 파일 경로
+     * @param dataString String 형태의 데이터
+     * @throws IOException
+     */
+    public static void writeStringNewLine2File(Path resultFilePath, String dataString) throws IOException {
+        writeString2File(resultFilePath, dataString + System.lineSeparator());
+    }
+    /**
+     * String 데이터를 파일에 쓴다.
+     * @param resultFilePath Path 파일 경로
+     * @param dataString String 형태의 데이터
+     * @throws IOException
+     */
+    public static void writeString2File(Path resultFilePath, String dataString) throws IOException {
+        Files.write(resultFilePath, dataString.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+    }
+
+
+    /**
+     * 파일 존재하면 지우고 씀
+     * @param resultFilePathString
+     * @param dataString
+     * @throws IOException
+     */
+    public static void writeString2NewFile(String resultFilePathString, String dataString) throws IOException {
+        Path resultFilePath = Paths.get(resultFilePathString);
+        Files.deleteIfExists(resultFilePath);
+
+        writeString2File(resultFilePath, dataString);
+    }
 }
