e9d6776ccec08d914a21d67626078896bf1d5dec
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2020-12-20 15:21:57 +0900

    Update EncodingUtils.java

--- EncodingUtils.java
+++ EncodingUtils.java
@@ -38,4 +38,31 @@ public class EncodingUtils {
         }
         return charset;
     }
+    
+    
+    // 유니코드에서 String으로 변환
+    public static String convertUnicode2String(String val) {
+        StringBuffer sb = new StringBuffer();
+        try {
+
+
+            for (int i = 0; i < val.length(); i++) {
+                // 조합이 \\u로 시작하면 6글자를 변환한다. \\uxxxx
+                if ('\\' == val.charAt(i) && 'u' == val.charAt(i + 1) && i > 0 && '\\' != val.charAt(i-1)) {
+                    // 그 뒤 네글자는 유니코드의 16진수 코드이다. int형으로 바꾸어서 다시 char 타입으로 강제 변환한다.
+                    Character r = (char) Integer.parseInt(val.substring(i + 2, i + 6), 16);
+                    sb.append(r);
+
+                    i += 5; // for의 증가 값 1과 5를 합해 6글자를 점프
+                } else {
+
+                    sb.append(val.charAt(i)); // ascii코드면 그대로 버퍼에 넣는다.
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            log.error(val);
+        }
+        return sb.toString();
+    }
 }
