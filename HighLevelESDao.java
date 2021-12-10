d4290c281eec1cfdec186b88ead8b9291834e151
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2021-12-10 14:37:41 +0900

    Update HighLevelESDao.java

--- HighLevelESDao.java
+++ HighLevelESDao.java
@@ -1,20 +1,26 @@
-package kr.co.saramin.lab.searchevaluator.dao;
+package kr.co.saramin.lab.autocompleteframework.dao;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.util.EntityUtils;
+import lombok.Setter;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest.AliasActions;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.*;
-import org.elasticsearch.client.*;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.core.CountRequest;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -23,16 +29,18 @@ import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+
 
 @Repository
 @RequiredArgsConstructor
+@Setter
 public class HighLevelESDao {
     private final ObjectMapper objectMapper;
-    private final Gson gson;
+    private RestHighLevelClient client;
 
     @Value("${elasticsearch.scroll-timout-ms}")
     private long scrollTimeoutMs;
+
 
     /**
      * 쿼리 검색
@@ -85,20 +93,6 @@ public class HighLevelESDao {
     }
 
     /**
-    * 쿼리 검색 결과를 ORM 으로 변환
-    *
-    * @param hits  searchReponse.getHits().getHits()
-    * @param clazz  Dto.class
-    * @param <T>
-    * @return
-    */
-    private <T> List<T> convertHitsToPojoList(SearchHit[] hits, Class<T> clazz) {
-        return Arrays.stream(hits)
-                .map(hit -> objectMapper.convertValue(hit.getSourceAsMap(), clazz))
-                .collect(Collectors.toList());
-    }
-    
-    /**
      * ORM 싱글 인덱싱
      * @param <T>
      * @param client
@@ -137,6 +131,38 @@ public class HighLevelESDao {
         }
         return client.bulk(bulkRequest, RequestOptions.DEFAULT);
     }
+    public <T> BulkResponse index(String indexName, List<T> documentList) throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        for (T document : documentList) {
+            IndexRequest indexRequest = new IndexRequest(indexName, "_doc");
+            indexRequest.source(objectMapper.writeValueAsString(document), XContentType.JSON);
+            bulkRequest.add(indexRequest);
+        }
+        return client.bulk(bulkRequest, RequestOptions.DEFAULT);
+    }
+
+
+    /**
+     * 벌크 업데이트
+     * _id 값이 같아야 업데이트됨, 아님 인서트되버림
+     * @param client
+     * @param index
+     * @param idList document의 _id. hits[i].getId() 로 구할수있다.
+     * @param documentList
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public <T> BulkResponse index(RestHighLevelClient client, String index, List<String> idList, List<T> documentList) throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        for (int i = 0; i < documentList.size(); i++) {
+            IndexRequest indexRequest = new IndexRequest(index, "_doc", idList.get(i));
+            indexRequest.source(objectMapper.writeValueAsString(documentList.get(i)), XContentType.JSON);
+            bulkRequest.add(indexRequest);
+        }
+
+        return client.bulk(bulkRequest, RequestOptions.DEFAULT);
+    }
 
     /**
      * 인덱스 존재 안할 시 생성
@@ -166,6 +192,73 @@ public class HighLevelESDao {
         return client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
     }
 
+
+    /**
+     * 인덱스 생성
+     * @param client
+     * @param indexName
+     * @return
+     * @throws IOException
+     */
+    private CreateIndexResponse createIndex(RestHighLevelClient client, String indexName, XContentBuilder settingsBuilder) throws IOException {
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+        createIndexRequest.settings(settingsBuilder);
+        return client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 인덱스 생성
+     * @param client
+     * @param indexName
+     * @return
+     * @throws IOException
+     */
+    private CreateIndexResponse createIndex(RestHighLevelClient client, String indexName, XContentBuilder settingsBuilder, XContentBuilder mappingBuilder) throws IOException {
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+        createIndexRequest.settings(settingsBuilder);
+        createIndexRequest.mapping("_doc", mappingBuilder);
+        return client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 강제 인덱스 생성 (분석기 적용 가능)
+     * 인덱스 이미 존재 시 삭제 후 생성
+     * @param client
+     * @param indexName
+     * @param settingsBuilder
+     * @param mappingBuilder
+     * @return
+     */
+    public CreateIndexResponse createIndexForcing(RestHighLevelClient client, String indexName, XContentBuilder settingsBuilder, XContentBuilder mappingBuilder) throws IOException {
+        deleteIndexIfExist(client, indexName);
+
+        return createIndex(client, indexName, settingsBuilder, mappingBuilder);
+    }
+
+    /**
+     * 인덱스 이미 존재 시 삭제
+     * @param client
+     * @param indexName
+     * @throws IOException
+     */
+    public void deleteIndexIfExist(RestHighLevelClient client, String indexName) throws IOException {
+        if(isExistIndex(client, indexName)) {
+            deleteIndex(client, indexName);
+        }
+    }
+
+    /**
+     * 인덱스 삭제
+     * @param client
+     * @param indexName
+     * @return
+     */
+    private AcknowledgedResponse deleteIndex(RestHighLevelClient client, String indexName) throws IOException {
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(indexName);
+        return client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+    }
+
+
     /**
      * 인덱스 존재 여부
      * @param client
@@ -179,25 +272,26 @@ public class HighLevelESDao {
     }
 
     /**
-     * 인덱스 전체 count 반환
-     * count 호출 전에 실행한 벌크 색인이 늦게 완료되면 count 동기화가 안될 수 있다..
-     * Highlevelclient6 이 ES7에선 totalHits가 0으로 떠서 low level 사용함
-     * @param client
+     * 인덱스 존재 여부
      * @param indexName
      * @return
      * @throws IOException
      */
-    public long count(RestHighLevelClient client, String indexName) throws IOException {
-        RestClient lowLevelClient = client.getLowLevelClient();
-        Response response = lowLevelClient.performRequest(new Request("GET", indexName + "/_count"));
-        String jsonBody = EntityUtils.toString(response.getEntity());
-        Map<String, Object> responseMap = gson.fromJson(jsonBody, Map.class);
-        return Double.valueOf((double) responseMap.get("count")).longValue();
+    public boolean isExistIndex(String indexName) throws IOException {
+        GetIndexRequest getIndexRequest = new GetIndexRequest().indices(indexName);
+        return client.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
     }
 
     public SearchResponse startScroll(RestHighLevelClient client, String[] indexNames, SearchSourceBuilder searchSourceBuilder) throws IOException {
         searchSourceBuilder.trackTotalHits(true);
         SearchRequest searchRequest = getSearchRequest(indexNames, searchSourceBuilder);
+        searchRequest.scroll(new Scroll(TimeValue.timeValueMillis(scrollTimeoutMs)));
+        return client.search(searchRequest, RequestOptions.DEFAULT);
+    }
+
+    public SearchResponse startScroll(RestHighLevelClient client, String indexName, SearchSourceBuilder searchSourceBuilder) throws IOException {
+        searchSourceBuilder.trackTotalHits(true);
+        SearchRequest searchRequest = getSearchRequest(indexName, searchSourceBuilder);
         searchRequest.scroll(new Scroll(TimeValue.timeValueMillis(scrollTimeoutMs)));
         return client.search(searchRequest, RequestOptions.DEFAULT);
     }
@@ -225,4 +319,43 @@ public class HighLevelESDao {
         countRequest.source(searchSourceBuilder);
         return client.count(countRequest, RequestOptions.DEFAULT).getCount();
     }
+
+    public long count(RestHighLevelClient client, String index) throws IOException {
+        CountRequest countRequest = new CountRequest(index);
+        return client.count(countRequest, RequestOptions.DEFAULT).getCount();
+    }
+
+    /**
+     * aliasName 의 설정된 모든 인덱스 제거 후 indexName 만 add
+     * @param aliasName
+     * @param indexName
+     * @return
+     */
+    public AcknowledgedResponse replaceAlias(String aliasName, String indexName) throws IOException {
+        IndicesAliasesRequest indicesAliasesRequest = new IndicesAliasesRequest();
+
+        // clear
+        AliasActions clearAliasActions = new AliasActions(AliasActions.Type.REMOVE)
+                .alias(aliasName)
+                .index("*");
+        indicesAliasesRequest.addAliasAction(clearAliasActions);
+        // add
+        AliasActions addAliasActions = new AliasActions(AliasActions.Type.ADD)
+                .alias(aliasName)
+                .index(indexName);
+        indicesAliasesRequest.addAliasAction(addAliasActions);
+
+        return client.indices().updateAliases(indicesAliasesRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * alias 존재 여부 검사
+     * @param aliasName
+     * @return
+     */
+    public boolean isExistAlias(String aliasName) throws IOException {
+        GetAliasesRequest getAliasesRequest = new GetAliasesRequest(aliasName);
+        return client.indices().existsAlias(getAliasesRequest, RequestOptions.DEFAULT);
+    }
+
 }
